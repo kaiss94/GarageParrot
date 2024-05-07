21884bb6e2898b35b75c267350562929b499e8ba
Issam Mezrani <kaiss94@hotmail.com>
2024-05-07 19:58:07 +0200

    corrections de bugs

--- views/header.php
+++ views/header.php
@@ -7,14 +7,14 @@ if (session_status() === PHP_SESSION_NONE) {
 
 <header class="container bg-danger p-3">
   <div id="logo" class="d-flex flex-column m-3 text-center">
-    <a href="#"><img src="../assets/images/logoSVG.svg" alt="logo" width="200" height="150"></a>
+    <a href="../views/index.php"><img src="../assets/images/logoSVG.svg" alt="logo" width="200" height="150"></a>
   </div>
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-      <button id="boutonMenu" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <button id="boutonMenu" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <div class="collapse navbar-collapse" id="navbarToggleExternalContent">
         <ul class="navbar-nav mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link text-light active" aria-current="page" href="../views/index.php">Accueil</a>
